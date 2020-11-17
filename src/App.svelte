70aecc29402daac1f6fb5bc14778f81e65f14243
Julian <julian.ollivieira@gmail.com>
2020-11-17 14:14:32 +0100

    Initial commit, spotify login button
    - Extract Svelte example zip.
    - Create Spotify login button with official spotify icon.

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<div class="container">
+	<div class="header">
+		<h1>Spotify statistics</h1>
+		<button>
+			<img src="spotify_Icon_RGB_White.png" alt="Spotify Icon">
+			<p>Login with Spotify</p>
+		</button>
+	</div>
+	<hr>
+</div>
